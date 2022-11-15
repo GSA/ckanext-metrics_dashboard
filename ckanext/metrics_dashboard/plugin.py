11782dec06abdf8098862faa1b01230f755b6164
Tyler Burton <tyler.burton@gsa.gov>
2022-11-15 15:26:52 -0600

    remove unused plugins

--- ckanext/metrics_dashboard/plugin.py
+++ ckanext/metrics_dashboard/plugin.py
@@ -12,25 +12,17 @@ class MetricsDashboard(p.SingletonPlugin, p.toolkit.DefaultDatasetForm):
     change, and will create a new ckanext.archiver celery task to archive the
     resource.
     """
-    p.implements(p.IDomainObjectModification, inherit=True)
     p.implements(IReport)
     p.implements(p.IConfigurer, inherit=True)
-    p.implements(p.ITemplateHelpers)
 
     # IReport
 
     def register_reports(self):
         """Register details of an extension's reports"""
         from ckanext.metrics_dashboard import reports
-        return [reports.metrics_dashboard_report_info,
-                ]
+        return [reports.metrics_dashboard_report_info]
 
     # IConfigurer
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
-
-    # ITemplateHelpers
-
-    def get_helpers(self):
-        return {}
