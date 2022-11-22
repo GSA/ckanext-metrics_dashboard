f2650f9794f1da03a34dcdd70c53088f5c9529c5
Tyler Burton <tyler.burton@gsa.gov>
2022-11-22 11:55:55 -0600

    nest harvest source request in try block

--- ckanext/metrics_dashboard/reports.py
+++ ckanext/metrics_dashboard/reports.py
@@ -51,72 +51,76 @@ def _get_harvest_results(organization=None):
 
     for source in query:
         id = source.id
-        source = p.toolkit.get_action("harvest_source_show")(
-            context, {"id": id}
-        )
-        if source['state'] == 'deleted':
+        try:
+            source = p.toolkit.get_action("harvest_source_show")(
+                context, {"id": id}
+            )
+
+            last_job = source['status']['last_job']
+            source_org = source['organization']
+            if not last_job:
+                last_job = {
+                    'created': 'N/A',
+                    'finished': 'N/A',
+                    'status': 'N/A',
+                    'stats': {
+                        'added': 'N/A',
+                        'updated': 'N/A',
+                        'not modified': 'N/A',
+                        'errored': 'N/A',
+                        'deleted': 'N/A',
+                    },
+                    'object_error_summary': 'N/A',
+                }
+            if not source_org:
+                source_org = {
+                    'name': 'N/A',
+                    'title': 'N/A',
+                }
+            row_data = OrderedDict((
+                ('name', source['name']),
+                ('metadata_created', source['metadata_created']),
+                ('source_type', source['source_type']),
+                ('state', source['state']),
+                ('frequency', source['frequency']),
+                ('organization_name', source_org['name']),
+                ('organization_title', source_org['title']),
+                ('job_count', source['status']['job_count']),
+                ('total_datasets', source['status']['total_datasets']),
+                ('last_job_created', last_job['created']),
+                ('last_job_finished', last_job['finished']),
+                ('last_job_status', last_job['status']),
+                ('last_job_added', last_job['stats']['added']),
+                ('last_job_updated', last_job['stats']['updated']),
+                ('last_job_not_modified', last_job['stats']['not modified']),
+                ('last_job_errored', last_job['stats']['errored']),
+                ('last_job_deleted', last_job['stats']['deleted']),
+                ('object_error_summary', last_job['object_error_summary']),  # not yet implemented
+            ))
+
+            table_data.append(row_data)  # needed for csv export
+
+            if (row_data['organization_name'] in table_data_by_org):
+                table_data_by_org[row_data['organization_name']]['harvest_sources'] += 1
+                table_data_by_org[row_data['organization_name']]['total_datasets'] += row_data['total_datasets']
+                # if we have an org, append the source data
+                if organization:
+                    table_data_by_org[row_data['organization_name']]['packages'].append(row_data)
+            else:
+                table_data_by_org[row_data['organization_name']] = {
+                    'organization_title': row_data['organization_title'],
+                    'harvest_sources': 1,
+                    'total_datasets': row_data['total_datasets'],
+                }
+                # if we have an org, append the source data
+                if organization:
+                    table_data_by_org[row_data['organization_name']]['packages'] = [row_data]
+        except BaseException as exception:
+            logging.exception(
+                f"An exception was thrown: {exception} ... While trying to process harvest source: {source.id} ")
             continue
-            # TODO: process deleted datasets so a logged in user can see those as well
-
-        last_job = source['status']['last_job']
-        source_org = source['organization']
-        if not last_job:
-            last_job = {
-                'created': 'N/A',
-                'finished': 'N/A',
-                'status': 'N/A',
-                'stats': {
-                    'added': 'N/A',
-                    'updated': 'N/A',
-                    'not modified': 'N/A',
-                    'errored': 'N/A',
-                    'deleted': 'N/A',
-                },
-                'object_error_summary': 'N/A',
-            }
-        if not source_org:
-            source_org = {
-                'name': 'N/A',
-                'title': 'N/A',
-            }
-        row_data = OrderedDict((
-            ('name', source['name']),
-            ('metadata_created', source['metadata_created']),
-            ('source_type', source['source_type']),
-            ('state', source['state']),
-            ('frequency', source['frequency']),
-            ('organization_name', source_org['name']),
-            ('organization_title', source_org['title']),
-            ('job_count', source['status']['job_count']),
-            ('total_datasets', source['status']['total_datasets']),
-            ('last_job_created', last_job['created']),
-            ('last_job_finished', last_job['finished']),
-            ('last_job_status', last_job['status']),
-            ('last_job_added', last_job['stats']['added']),
-            ('last_job_updated', last_job['stats']['updated']),
-            ('last_job_not_modified', last_job['stats']['not modified']),
-            ('last_job_errored', last_job['stats']['errored']),
-            ('last_job_deleted', last_job['stats']['deleted']),
-            ('object_error_summary', last_job['object_error_summary']),  # not yet implemented
-        ))
-
-        table_data.append(row_data)  # needed for csv export
-
-        if (row_data['organization_name'] in table_data_by_org):
-            table_data_by_org[row_data['organization_name']]['harvest_sources'] += 1
-            table_data_by_org[row_data['organization_name']]['total_datasets'] += row_data['total_datasets']
-            # if we have an org, append the source data
-            if organization:
-                table_data_by_org[row_data['organization_name']]['packages'].append(row_data)
-        else:
-            table_data_by_org[row_data['organization_name']] = {
-                'organization_title': row_data['organization_title'],
-                'harvest_sources': 1,
-                'total_datasets': row_data['total_datasets'],
-            }
-            # if we have an org, append the source data
-            if organization:
-                table_data_by_org[row_data['organization_name']]['packages'] = [row_data]
+            # some datasets error out and default to an internal url.
+            # TODO: process these datasets with authorization errors into a separate table
 
     return {
         'table': table_data,  # needed for csv export
